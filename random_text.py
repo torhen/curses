01fd5b20e972de14b381a810621c48f1b0007631
torhen <torhen@gmail.com>
2025-05-24 23:01:12 +0200

    Add files via upload

--- random_text.py
+++ random_text.py
@@ -1,6 +1,7 @@
 import curses
 import datetime
 import random
+import time
 
 class App:
     def __init__(self, win):
@@ -11,34 +12,35 @@ class App:
         for y in range(self.ymax):
             self.doc.append([])
             for x in range(self.xmax):
-                self.doc[y].append('*' * self.xmax)
+                self.doc[y].append(' ' * (self.xmax-1))
         self.win.nodelay(True)
         self.run()
 
     def update(self):
-        for y in range(self.ymax):
-            for x in range(self.xmax):
-                self.doc[y][x] = random.choice('abcdeghijklmnopqurstuvxyz      ')
-        s = str(datetime.datetime.now())
-        self.doc[0][0:len(s)] = s
+       
+        for y in range(self.ymax-1, 0, -1):
+             for x in range(self.xmax):
+                self.doc[y][x] = self.doc[y-1][x]
+
+        for x in range(self.xmax):
+            self.doc[0][x] = random.choice('abcdefghijklmnopqrstuvwxyz      ')
+
 
     def draw(self):
 
         inp = self.win.getch()
         if inp != curses.KEY_RESIZE:
                 self.win.clear()
-        out = ''
 
         height, width = self.win.getmaxyx()
-        for y in range(height-1):
-            for x in range(width-1):
+        for y in range(height-2):
+            for x in range(width-2):
                 if y < self.ymax and x < self.xmax:
-                    out = out + self.doc[y][x]
-            out = out + '\n'
-
-        # only one string as output
-        self.win.addstr(0, 0, out)
+                    self.win.addstr(y, x, self.doc[y][x])
+                else:
+                    self.win.addstr(y, x, ' ')
         self.win.refresh()
+        time.sleep(0.5)
 
     def run(self):
         while True:
